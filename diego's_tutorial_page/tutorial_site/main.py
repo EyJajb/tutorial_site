2f4f96b75190e2234b89e1ad2a3974f5c1a956aa
EyJajb <72881954+EyJajb@users.noreply.github.com>
2020-12-18 20:11:07 -0800

    Update main.py

--- diego's_tutorial_page/tutorial_site/main.py
+++ diego's_tutorial_page/tutorial_site/main.py
@@ -1,7 +1,16 @@
-#import projects #projects definitions are placed in different file
+import projects #projects definitions are placed in different file
 #import data
+# Import modules for CGI handling
+import cgi, cgitb
 
-#import data
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+# Get data from fields
+name = form.getvalue('name')
+email = form.getvalue('email')
+class1 = form.getvalue('class 1')
+class2 = form.getvalue('class 2')
+class3 = form.getvalue('class 3')
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template
@@ -10,7 +19,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_route():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects.setup())
+@app.route('/test/')
+def test_route():
+    return render_template("test.html", projects=projects.setup())
+
+
+
 
 
 if __name__ == "__main__":
