e45998f0b77268c33ef0fee188f1683271ee9e71
luca pinto <luca_pinto@yahoo.com>
2021-01-07 21:07:23 -0800

    yurrr

--- diego's_tutorial_page/tutorial_site/main.py
+++ diego's_tutorial_page/tutorial_site/main.py
@@ -13,7 +13,8 @@ class2 = form.getvalue('class 2')
 class3 = form.getvalue('class 3')
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
+import os
 #create a Flask instance
 app = Flask(__name__)
 
@@ -24,6 +25,25 @@ def home_route():
 def test_route():
     return render_template("test.html", projects=projects.setup())
 
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    if request.method == 'GET':
+        return render_template("form.html")
+    elif request.method == 'POST':
+        if request.form.get('title') != '' and request.form.get('category') != '' and request.form.get('description') != '' :
+            title = request.form.get('title')
+            category = request.form.get('category')
+            description = request.form.get('description')
+            return redirect("/showform?title=" + str(title) +"&category=" + str(category) + "&description=" + str(description) + "&video=" + str(video), code=302)
+        else:
+            return render_template("form.html")
+
+@app.route('/showform', methods=['POST', 'GET'])
+def showform():
+    title = request.args.get('title')
+    category = request.args.get('category')
+    description = request.args.get('description')
+    return render_template("showform.html", title=title , description=description , category=category)
 
 
 
