9a13b2303383c710923e1d17eefc16aa30004274
EyJajb <72881954+EyJajb@users.noreply.github.com>
2021-01-16 09:55:09 -0800

    Create data.py

---
+++ diego's_tutorial_page/tutorial_site/data.py
@@ -0,0 +1,99 @@
+name = "AP Calculus AB"
+header = "Time to learn"
+author = "Naweid"
+calc = [{"topic": "Limits",
+         "img": "https://www.mathsisfun.com/calculus/images/discontinuous-function.svg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "Limits are the first thing that are taught in Calculus and is crucial to understanding the basics. imits can be used to understand derivative, integrals, and other theorems which all play a huge part in Calculus. Limits are the value that a function is approaching towards."
+         },
+        {"topic": "Derivatives",
+         "img": "https://www.onlinemathlearning.com/image-files/basic-derivative-rules.png",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "Derivatives is one of the main topics that stays consisten throughout the entire AP Course. There are tons of formulas to finding the derivative, but this can also be applied to other problems and ideas. Derivatives can be used to find critical points and how their derivative counterpart graph looks like."
+         },
+        {"topic": "Mean Value Theorem",
+         "img": "https://i.ytimg.com/vi/SL2RobwU_M4/maxresdefault.jpg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Mean Value Theorem is a pretty simple topics as there is only one formula to remember. The mean value theorem shows that there is a 'c' value within the points A and B. This occurs when the tangent at this point is parallel with the secant line."
+         }
+        ]
+euro = [{"topic": "The French Revolution",
+         "img": "https://www.mathsisfun.com/calculus/images/discontinuous-function.svg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The French Revolution is one the biggest chapters in AP Euro history and can also be considered one of the hardest. This chapter will explain the fall of the old regime and how it went through its different phases in order to get where it is today."
+         },
+        {"topic": "",
+         "img": "https://www.onlinemathlearning.com/image-files/basic-derivative-rules.png",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The enlightenment was an intense intellectual movement which inspired the minds of many and sparked the inventions which made the world where we are at now. Big names such as Newton and Locke appeared and made a big impact. "
+         },
+        {"topic": "The Holocaust",
+         "img": "https://i.ytimg.com/vi/SL2RobwU_M4/maxresdefault.jpg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Holocaust is one of the most known events out there and one of the most important too. The death of millions of Jewish brought a lot of attention and changed how society is viewed today."
+         }
+        ]
+hist = [{"topic": "Jim Crow Laws",
+         "img": "https://media.nationalgeographic.org/assets/photos/135/255/65c1538e-f51f-4a8d-ac64-914c0a722474.jpg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Jim Crow Laws followed the 14th amendment, creating lives for black people harder and putting a lot more restrictions on daily life. Education was a huge disadvantage for black people as they weren't able to go to higher up schools to study their interests. "
+         },
+        {"topic": "Great Depression",
+         "img": "https://www.thoughtco.com/thmb/pBvhAwW1krYA2bQGrTITshOPkKc=/813x813/smart/filters:no_upscale()/Migrant_Mother_Nipomo_California_3334095096-37e37c052a0745ba9cf9fae3cc5f967b.jpg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Great Depression was in the 1930s, which occured after the stock market crash. It was the biggest economic downfall of the country and many people went into poverty."
+         },
+        {"topic": "World War 1",
+         "img": "https://cdn.britannica.com/44/65944-050-F18FEEA4/soldier-British-trench-Western-Front-World-War.jpg",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "World War 1 began in 1914 after the assissination of Archduke Ferdinand. As tensions rose, more attacks began to happen between the countries. After a few encounters with Germany, U.S entered the war. "
+         }
+        ]
+phys = [{"topic": "Kinematic Equations",
+         "img": "https://www.physicsclassroom.com/Class/1DKin/U1L6a1.gif",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Kinematic Equations are 4 equations that are relationships between initial and final velocity, time, distance and acceleration. They help simplify the calculations  of these different pneumonia. Here is a video tutorial that provides more explaining and practice problems: https://www.youtube.com/watch?v=Po7li9JbEsQ"
+         },
+        {"topic": "Kinematic Equations",
+         "img": "https://www.physicsclassroom.com/Class/1DKin/U1L6a1.gif",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Kinematic Equations are 4 equations that are relationships between initial and final velocity, time, distance and acceleration. They help simplify the calculations of these different pneumonia. Here is a video tutorial that provides more explaining and practice problems: https://www.youtube.com/watch?v=Po7li9JbEsQ"
+         },
+        {"topic": "Kinematic Equations",
+         "img": "https://www.physicsclassroom.com/Class/1DKin/U1L6a1.gif",
+         "sizex": 1,"sizey": 1,
+         "video": "none", #put none if no video is included
+         "description": "The Kinematic Equations are 4 equations that are relationships between initial and final velocity, time, distance and  acceleration. They help simplify the calculations of these different pneumonia. Here is a video tutorial that provides more explaining and practice problems: https://www.youtube.com/watch?v=Po7li9JbEsQ"
+         }
+        ]
+pcalc = [{"topic": "Trig",
+          "img": "https://www.mathsisfun.com/calculus/images/discontinuous-function.svg",
+          "sizex": 1,"sizey": 1,
+          "video": "none", #put none if no video is included
+          "description": "In Integrated 3B, we learned the basics of trigonometry, but now we are going further and applying them to problems. There are much more than sin, cos, and tan as there will be the introduction of many different formulas and equations."
+          },
+         {"topic": "Logarithmic Functions",
+          "img": "https://www.onlinemathlearning.com/image-files/basic-derivative-rules.png",
+          "sizex": 1,"sizey": 1,
+          "video": "none", #put none if no video is included
+          "description": "Logarithmic Functions is the power to which a number must be raised in order to get some other number. This is a simple topic to learn and isn’t widely used throughout calc."
+          },
+         {"topic": "Parametric Equations",
+          "img": "https://i.ytimg.com/vi/SL2RobwU_M4/maxresdefault.jpg",
+          "sizex": 1,"sizey": 1,
+          "video": "none", #put none if no video is included
+          "description": "Parametric Equations any of a set of equations that express the coordinates of the points of a curve as functions of one parameter or that express the coordinates of the points of a surface as functions of two parameters."
+          }
+         ]
+
