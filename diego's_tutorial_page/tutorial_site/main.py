e1bc5fadfe025d3c2925efae1fb620f7b8345bab
EyJajb <72881954+EyJajb@users.noreply.github.com>
2021-01-21 00:22:38 -0800

    Update main.py

--- diego's_tutorial_page/tutorial_site/main.py
+++ diego's_tutorial_page/tutorial_site/main.py
@@ -2,22 +2,21 @@ import projects #projects definitions are placed in different file
 #import data
 # Import modules for CGI handling
 import cgi, cgitb
-from data import *
+import mysql.connector
+
+
+mydb = students_tbl.connector.connect(
+    students="students_id",
+    password="password",
+)
 
 # Create instance of FieldStorage
 from pip._vendor import requests
 
-from views.restapi import restapi_bp
+from __init__ import restapi_bp
 from flask import render_template
-from models.lessons import menus
 import requests
-form = cgi.FieldStorage()
-# Get data from fields
-name = form.getvalue('name')
-email = form.getvalue('email')
-class1 = form.getvalue('class 1')
-class2 = form.getvalue('class 2')
-class3 = form.getvalue('class 3')
+
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
@@ -25,23 +24,49 @@ import os
 #create a Flask instance
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_route():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template("home.html", projects=projects.setup())
+    return render_template("home.html", projects=projects.setup())
+    #if not session.get('logged_in'):
+     #   return render_template('login.html')
+    #else:
+     #   return render_template("home.html", projects=projects.setup())
 @app.route('/test/')
 def test_route():
     return render_template("test.html", projects=projects.setup())
 
 @app.route('/login', methods=['POST', 'GET'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('Wrong password!')
-    return home_route()
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO students_tbl (students_id, password) VALUES (%s, %s)"
+    val = (request.form['username'] , request.form['password'])
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+    return render_template('login.html', )
+
+    print(request.form['username'])
+    print(request.form['password'])
+    #if request.form['password'] == 'password' and request.form['username'] == 'admin':
+     #   session['logged_in'] = True
+    #else:
+     #   flash('Wrong password!')
+    #return home_route()
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    if request.method == 'GET':
+        return render_template("form.html")
+    elif request.method == 'POST':
+        if request.form.get('title') != '' and request.form.get('category') != '' and request.form.get('description') != '' :
+            title = request.form.get('title')
+            category = request.form.get('category')
+            description = request.form.get('description')
+            return redirect("/showform?title=" + str(title) +"&category=" + str(category) + "&description=" + str(description) + "&video=" + str(video), code=302)
+        else:
+            return render_template("form.html")
 
 @app.route('/calc/')
 def calc_route():
@@ -58,19 +83,6 @@ def hist_route():
 @app.route('/precalc/')
 def precalc_route():
     return render_template("Class.html", projects=projects.setup(), data1=pcalc)
-
-@app.route('/form', methods=['POST', 'GET'])
-def form():
-    if request.method == 'GET':
-        return render_template("form.html")
-    elif request.method == 'POST':
-        if request.form.get('title') != '' and request.form.get('category') != '' and request.form.get('description') != '' :
-            title = request.form.get('title')
-            category = request.form.get('category')
-            description = request.form.get('description')
-            return redirect("/showform?title=" + str(title) +"&category=" + str(category) + "&description=" + str(description) + "&video=" + str(video), code=302)
-        else:
-            return render_template("form.html")
 
 @app.route('/showform', methods=['POST', 'GET'])
 def showform():
